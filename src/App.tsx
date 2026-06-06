bc0b7f6c496e18f3e789d8ca15bedd8583d44665
DatsOlena <datsolena@gmail.com>
2026-06-06 20:48:20 +0200

    App updates

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+
+function App() {
+
+
+  return (
+    <>
+      <Navigation />
+      <Hero />
+    </>
+  )
+}
+
+export default App
