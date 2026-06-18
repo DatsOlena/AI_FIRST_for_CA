128063f5831df4986c2621cd2ffcfe79ae8b27bb
DatsOlena <datsolena@gmail.com>
2026-06-18 20:39:58 +0200

    Wire AboutUs and PainPoints sections into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Navigation from './components/Navigation'
 import Hero from './components/Hero'
+import PainPoints from './components/PainPoints'
+import AboutUs from './components/AboutUs'
 
 function App() {
-
-
   return (
     <>
       <Navigation />
       <Hero />
+      <PainPoints />
+      <AboutUs />
     </>
   )
 }
